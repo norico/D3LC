c34d66b40b175dd2bed9377932e3a231fb510b4b
norico <norico.legrand@gmail.com>
2021-04-26 21:03:59 +0200

    V 2.4.3
    Interface optimisation

--- elctron-app.js
+++ elctron-app.js
@@ -1,13 +1,17 @@
 const env = require('dotenv')
-const {app, BrowserWindow, ipcMain, globalShortcut } = require('electron')
+const {app, BrowserWindow, ipcMain, globalShortcut, screen, Notification, Tray, Menu  } = require('electron')
 const path = require('path')
 const {shortcuts, config} = require('./config')
 const server = require('./server-app')
 
 let mainWindow = null
+let tray = null
+
 app.on('ready', ()=>{
     createGlobalShortcuts()
     createMainWindow()
+    createTray()
+    setWindowPos()
 })
 app.on('window-all-closed', () => {
     app.quit()
@@ -25,11 +29,12 @@ function createGlobalShortcuts(){
     })
 }
 function createMainWindow(){
+    console.log(process.type)
     mainWindow = new BrowserWindow({
         show:false,
         autoHideMenuBar:true,
-        width: 200,
-        height: 200,
+        width: 180,
+        height: 180,
         resizable:false,
         maximizable:true,
         maximizable:false,
@@ -42,13 +47,58 @@ function createMainWindow(){
         }
     })
     mainWindow.loadURL(`file://${__dirname}/index.html`)
-    mainWindow.show()
     //mainWindow.webContents.openDevTools()
+
+
 }
+function createTray(counter){
+    tray = new Tray( config.icon )
+    trayEvent()
+}
+
+function setWindowPos() {
+    const {width, height} = screen.getPrimaryDisplay().workAreaSize
+    mainWindow.setBounds({
+        x: width - mainWindow.getSize()[0] - 5,
+        y: height - mainWindow.getSize()[1] - 5
+    })
+}
+
+function trayEvent(){
+    const isMac = process.platform === 'darwin'
+    tray.setToolTip(config.name+' '+ app.getVersion())
+    tray.displayBalloon({
+        'title' : config.name,
+        'content' : app.getVersion(),
+        'largeIcon' : config.icon,
+        'respectQuietTime' : true
+    })
+    tray.setContextMenu(Menu.buildFromTemplate([
+        { 'label' : app.name+' '+app.getVersion(), 'enabled' : false },
+        //{ role: 'toggleDevTools', 'visible': true },
+        { type: 'separator' },
+        isMac ? { role: 'close' } : { role: 'quit' }
+    ]))
+    tray.on('click', (event)=>{
+        mainWindow.isVisible() === true ? mainWindow.hide() : mainWindow.show()
+    })
+}
+
+/* IPC */
+
 ipcMain.on('counter:init', () => {
     mainWindow.send('message', 'init')
 })
+
 ipcMain.on('counter:value', (event, counter_value) => {
     server.send(counter_value)
     global.counter = counter_value
+    /*
+    let notify  = new Notification({
+        'title' : app.name,
+        'icon' : config.icon,
+    })
+    notify.body = counter_value.toString()
+    // notify.show()
+    */
 })
